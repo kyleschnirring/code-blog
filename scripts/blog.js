ff7b9c6996fe2c623a60562199327c4e76a7c1c0
kyleschnirring <kyleschnirring@icloud.com>
2016-01-09 11:28:38 -0800

    added images and tried to create a navbar

--- scripts/blog.js
+++ scripts/blog.js
@@ -5,14 +5,21 @@ function Project (object) {
   this.author = object.author;
   this.date = object.date;
   this.projectUrl = object.projectUrl;
+  this.image = object.image;
   this.body = object.body;
 }
 
 Project.prototype.toHtml = function() {
   var $newProject = $('article.template').clone();
+  $newProject.find('h1').text(this.title);
+  $newProject.find('address').text(this.author);
+  $newProject.find('a').attr('href', this.projectUrl).text(this.projectUrl);
   $newProject.find('time[pubdate]').attr('title', this.date);
   $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.date))/60/60/24/1000) + ' days ago');
+  $newProject.find('.projectImages').attr('src', this.image);
+  $newProject.find('.article-body').html(this.body);
   $newProject.append('<hr>');
+  $newProject.removeClass('template');
   return $newProject;
 }
 
--- scripts/myprojects.js
+++ scripts/myprojects.js
@@ -5,6 +5,7 @@ myProject.rawData = [
     title:       '[tattle]',
     author:      'Kyle Schnirring',
     projectUrl:   'http:donttattleonme.com',
+    image: "images/tattle.png",
     date: '2014-11-05',
     body:        '<p>I really dont know what I want to write yet.</p>'
   },
@@ -12,6 +13,7 @@ myProject.rawData = [
     title:       'My First App',
     author:      'Kyle Schnirring',
     projectUrl:   'https://itunes.apple.com/us/app/profappltechinc/id713762905?mt=8',
+    image: "images/pat.png",
     date: '2013-11-04',
     body:     "<p>What can I say this was the first app I ever made, I made this for my father-in-laws company. It's not much but hey everyone has to start somewhere.</p>"
   }
